e6b860c2f43edb8335444638e21ae9d1374b4d7c
sony9154 <emus-04reunion@icloud.com>
2023-07-08 02:36:31 +0800

    import flashy_tab_bar2 pub

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:top_100_movies/models/movie.dart';
 import 'package:top_100_movies/views/widgets/movie_card.dart';
-
 import '../models/movie.api.dart';
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 
  class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +11,22 @@ import '../models/movie.api.dart';
  }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Favorite',
+      style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Profile',
+      style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+    ),
+  ];
 
   List<Movie> _movies = [];
   bool _isLoading = true;
@@ -54,6 +70,26 @@ class _HomePageState extends State<HomePage> {
                       rating: _movies[index].rating,
                       thumbnail: _movies[index].image);
                 },
-              ));
+              ),
+      bottomNavigationBar: FlashyTabBar(
+        selectedIndex: _selectedIndex,
+        showElevation: true,
+        onItemSelected: (index) => setState(() => _selectedIndex = index),
+        items: [
+          FlashyTabBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          FlashyTabBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorite'),
+          ),
+          FlashyTabBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+      ),          
+    );
   }
 }
